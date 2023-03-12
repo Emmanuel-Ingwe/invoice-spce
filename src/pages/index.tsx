3d4c2c49b211b875825c9dde8ecb49e71aa7a48d
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-03-12 14:30:34 +0100

    rr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<>
-			<h1 className='bg-red-500'>Next</h1>
+			<h1 className='bg-red-600'>Next</h1>
 		</>
 	);
 }
